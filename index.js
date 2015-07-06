b605e721b035860c0e2229cd9411f917efaf8514
Connor McKelvey <connormckelvey@gmail.com>
2015-07-05 22:33:40 -0400

    refactored shit

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 var app = require('./lib/cliche');
 
+
+app.command('default', {
+	desc: 'This is the default command',
+	controller: 'test'
+})
+
 app.command('git', {
-	alias: 'g',
+	desc: 'Version control command',
 	controller: 'test',
-	prompt: 'prompts/init'
+	params: [
+		{ 
+			name: 'test',
+			alias: 't',
+			required: true,
+			validation: 'boolean'
+		},
+		{                                     
+			name: 'url',
+			alias: 'u', 
+			required: false,
+			validation: 'string'
+		}
+	]
 });
 
-app.controller('test', function(){
+// app.command('git.remote', {
+// 	controller: 'rteest',
+// 	prompt: 'prompts/init'
+// });
+
+app.controller('test', function(options){
+
+	setTimeout(function(){
+		console.log(options);
+	}, 2000);
+});
+
+
+app.controller('rteest', function(options){
 	console.log('lolasdasdasdasds');
 });
 
-app.start();
+
+app.run();
